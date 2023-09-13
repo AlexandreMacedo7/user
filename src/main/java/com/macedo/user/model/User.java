044bd355c9e5ca550b00714f4a3394e780d8431e
Alexandre Macedo <alexandre.cst.macedo@gmail.com>
2023-09-12 22:21:17 -0400

    Criada Entidade User

---
+++ src/main/java/com/macedo/user/model/User.java
@@ -0,0 +1,27 @@
+package com.macedo.user.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity(name = "usuario")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @NotBlank
+    private String name;
+    @NotBlank
+    private String email;
+}
