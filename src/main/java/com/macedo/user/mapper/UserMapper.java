8458b2d18f4ef3cbbe377579476ca2541741ce1e
Alexandre Macedo <alexandre.cst.macedo@gmail.com>
2023-09-12 22:24:55 -0400

    Criado UserMapper

---
+++ src/main/java/com/macedo/user/mapper/UserMapper.java
@@ -0,0 +1,25 @@
+package com.macedo.user.mapper;
+
+import com.macedo.user.model.User;
+import com.macedo.user.model.dto.CreateUserDto;
+import com.macedo.user.model.dto.DetailsUserDto;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+public class UserMapper {
+
+    public User toEntity(CreateUserDto createUserDto) {
+        User user = new User();
+        user.setName(createUserDto.name());
+        user.setEmail(createUserDto.email());
+
+        return user;
+    }
+
+    public DetailsUserDto toDTO(User user) {
+        DetailsUserDto dto = new DetailsUserDto(user);
+        return dto;
+    }
+}
