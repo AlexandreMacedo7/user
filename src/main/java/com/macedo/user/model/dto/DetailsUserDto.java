f71ad866cd89c3094a3ff9670786b53c479f5377
Alexandre Macedo <alexandre.cst.macedo@gmail.com>
2023-09-12 22:23:57 -0400

    Criado dto para detalhes de user

---
+++ src/main/java/com/macedo/user/model/dto/DetailsUserDto.java
@@ -0,0 +1,10 @@
+package com.macedo.user.model.dto;
+
+import com.macedo.user.model.User;
+
+public record DetailsUserDto(Long id, String name, String email) {
+
+    public DetailsUserDto(User user){
+        this(user.getId(), user.getName(), user.getEmail());
+    }
+}
