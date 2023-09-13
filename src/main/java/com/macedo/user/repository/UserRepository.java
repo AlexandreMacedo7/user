94867f9b0276ff7923c843e0e3d6116ddc7318b0
Alexandre Macedo <alexandre.cst.macedo@gmail.com>
2023-09-12 22:22:09 -0400

    Criada UserRepository

---
+++ src/main/java/com/macedo/user/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.macedo.user.repository;
+
+import com.macedo.user.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
