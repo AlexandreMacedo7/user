92bc24383ef6e75dc6a747d0cf18eb4b01e63112
Alexandre Macedo <alexandre.cst.macedo@gmail.com>
2023-09-28 12:14:02 -0400

    feat: Deleta DTO AuthenticatedDataDTO e cria AuthenticatedDTO

---
+++ src/main/java/com/macedo/user/infra/security/securityDTOs/AuthenticatedDTO.java
@@ -0,0 +1,4 @@
+package com.macedo.user.infra.security.securityDTOs;
+
+public record AuthenticatedDTO(String login, String password) {
+}
--- src/main/java/com/macedo/user/model/dto/AuthenticatedDataDTO.java
+++
@@ -1,4 +0,0 @@
-package com.macedo.user.model.dto;
-
-public record AuthenticatedDataDTO(String login, String password) {
-}
