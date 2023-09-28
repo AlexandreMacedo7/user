531f35fa4210ecc2a8247989dd323927c2800d80
Alexandre Macedo <alexandre.cst.macedo@gmail.com>
2023-09-28 15:40:23 -0400

    refactor: Alteração no metodo login.
    Mudança no tipo de recebimento de dto, e implementação de chamadas retorno de token.

--- src/main/java/com/macedo/user/controller/LoginController.java
+++ src/main/java/com/macedo/user/controller/LoginController.java
@@ -1,8 +1,11 @@
 package com.macedo.user.controller;
 
-import com.macedo.user.model.dto.AuthenticatedDataDTO;
+import com.macedo.user.infra.security.securityDTOs.AuthenticatedDTO;
+import com.macedo.user.infra.security.securityDTOs.DataTokenJWTDTO;
+import com.macedo.user.model.Login;
 import com.macedo.user.model.dto.CreateLoginDTO;
-import com.macedo.user.service.LoginAuthenticatorService;
+import com.macedo.user.infra.security.securityService.AuthenticatorService;
+import com.macedo.user.infra.security.securityService.TokenService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -18,13 +21,17 @@ public class LoginController {
     private AuthenticationManager manager;
 
     @Autowired
-    private LoginAuthenticatorService service;
+    TokenService tokenService;
+
+    @Autowired
+    private AuthenticatorService service;
 
     @PostMapping
-    public ResponseEntity login(@RequestBody @Valid AuthenticatedDataDTO data){
-        var token = new UsernamePasswordAuthenticationToken(data.login(), data.password());
-        var authentication = manager.authenticate(token);
-        return ResponseEntity.ok().build();
+    public ResponseEntity login(@RequestBody @Valid AuthenticatedDTO data){
+        var authenticationToken = new UsernamePasswordAuthenticationToken(data.login(), data.password());
+        var authentication = manager.authenticate(authenticationToken);
+        var tokenJWT = tokenService.generateToken((Login) authentication.getPrincipal());
+        return ResponseEntity.ok(new DataTokenJWTDTO(tokenJWT));
     }
 
     @PostMapping("/create")
