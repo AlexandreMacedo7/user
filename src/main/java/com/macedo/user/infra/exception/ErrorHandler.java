9f22f29415f9aa4e08e9ffd2fba193601b68c732
Alexandre Macedo <alexandre.cst.macedo@gmail.com>
2023-09-29 21:38:31 -0400

    feat: Feito Classe de ErrorHandler
    Feito classe para tratamento de erros 500 e de validações não preenchidas

---
+++ src/main/java/com/macedo/user/infra/exception/ErrorHandler.java
@@ -0,0 +1,29 @@
+package com.macedo.user.infra.exception;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ErrorHandler {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity error404() {
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity error400(MethodArgumentNotValidException ex) {
+        var errors = ex.getFieldErrors();
+        return ResponseEntity.badRequest().body(errors.stream().map(DataErrorValidation::new).toList());
+    }
+
+    private record DataErrorValidation(String field, String message) {
+        public DataErrorValidation(FieldError error) {
+            this(error.getField(), error.getDefaultMessage());
+        }
+    }
+}
